6304ece55acc0c3502f3f86f52629f924f267891
lucaasmeiraf <lucaas.meiraf@gmail.com>
2024-04-16 21:26:10 -0300

    Using the local storage to keep the card games on screen as we reload the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
 
 function App() {
-  const [games, setGames] = useState([])
+  const [games, setGames] = useState(() => {
+    const storedGames = localStorage.getItem("biblioteca-de-jogos")
+    if(!storedGames) return []
+    return JSON.parse(storedGames)
+  })
   const [title, setTitle] = useState('')
   const [cover, setCover] = useState('')
 
   const addGame = ({ title, cover }) => {
     const id = Math.floor(Math.random() * 1000000)
     const game = { id, title, cover }
-    setGames(state => [...state, game])
+    setGames(state => {
+      const newState = [...state, game]
+      localStorage.setItem("biblioteca-de-jogos", JSON.stringify(newState))
+      return newState
+    })
   }
 
   const removeGame = (id) => {
-    setGames(state => state.filter(game => game.id !== id))
+    setGames(state => {
+      const newState = state.filter(game => game.id !== id)
+      localStorage.setItem("biblioteca-de-jogos", JSON.stringify(newState))
+      return newState
+    })
   }
 
   const handleSubmit = (ev) => {
