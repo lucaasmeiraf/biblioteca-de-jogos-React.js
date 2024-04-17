ce3e036296f01847a6a91804480b1bcd73483a71
lucaasmeiraf <lucaas.meiraf@gmail.com>
2024-04-16 21:16:31 -0300

    Adding the forms element and necessaries functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState([])
+  const [title, setTitle] = useState('')
+  const [cover, setCover] = useState('')
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.floor(Math.random() * 1000000)
+    const game = { id, title, cover }
+    setGames(state => [...state, game])
+  }
+
+  const removeGame = (id) => {
+    setGames(state => state.filter(game => game.id !== id))
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    addGame({ title, cover })
+    setTitle('')
+    setCover('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1>Biblioteca de Jogos</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Título:</label>
+          <input 
+            type="text" 
+            name='title' 
+            id='title'
+            value={title}
+            onChange={(ev) => setTitle(ev.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa (URL):</label>
+          <input 
+            type="text" 
+            name='cover'
+            id='cover'
+            value={cover}
+            onChange={(ev) => setCover(ev.target.value)}
+          />
+        </div>
+        <button>Adicionar à Biblioteca</button>
+      </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt="Capa do Jogo" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={() => removeGame(game.id)}>Remover</button>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
